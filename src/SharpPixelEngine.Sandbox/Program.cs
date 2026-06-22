98118520a31ce1fc3735fe320a6db00f19ac4390
MajkRitcherd <misutak87@gmail.com>
2026-06-22 09:42:34 +0200

    Add demo sandbox app (draws noise)

--- src/SharpPixelEngine.Sandbox/Program.cs
+++ src/SharpPixelEngine.Sandbox/Program.cs
@@ -1,1 +1,6 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using SharpPixelEngine.Sandbox;
+
+var app = new SandboxAppDemo();
+
+app.Construct(1280, 720);
+app.Start(1280, 720, "Sandbox App");
---
+++ src/SharpPixelEngine.Sandbox/SandboxAppDemo.cs
@@ -0,0 +1,37 @@
+﻿using SharpPixelEngine.Core.Graphics;
+
+namespace SharpPixelEngine.Sandbox
+{
+    /// <summary>
+    /// A sandbox application demonstrating the basic rendering capabilities
+    /// of the custom pixel engine.
+    /// </summary>
+    internal class SandboxAppDemo : Core.PixelGameEngine
+    {
+        private readonly Random _rnd = new();
+
+        /// <inheritdoc/>
+        protected override void OnUserCreate()
+        {
+            // Initialization logic goes here.
+            // Left empty for this demo as no assets or entities need to be loaded.
+        }
+
+        /// <inheritdoc/>
+        protected override void OnUserUpdate(float deltaTime)
+        {
+            // Noise output
+            for (int x = 0; x < ScreenWidth; x++)
+            {
+                for (int y = 0; y < ScreenHeight; y++)
+                {
+                    byte r = (byte)_rnd.Next(256);
+                    byte g = (byte)_rnd.Next(256);
+                    byte b = (byte)_rnd.Next(256);
+
+                    Draw(x, y, new PixelColor(r, g, b));
+                }
+            }
+        }
+    }
+}
